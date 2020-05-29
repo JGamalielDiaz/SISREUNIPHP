ad4d17ce288d5ddd9da227761288a374547db8ab
JGamalielDiaz <jgamalieldiaz27@gamail.com>
2020-05-29 13:25:32 -0600

    Se crea una nueva Ruta la cual nos permite ver los Roles de Aseo

--- routes/api.php
+++ routes/api.php
@@ -37,5 +37,24 @@ Route::get('EstuList',function(){
             })
             ->rawColumns(['btn'])
             ->toJson();
+});
 
+Route::get('RolesPendientes',function(){
+
+    $data = DB::table('tbl_persona AS per')
+    ->join('TBL_Estudiante AS estu','per.Per_ID','=','estu.Est_ID')
+
+    ->join('tbl_histoestucuarto as hsc','estu.Est_ID','=','hsc.Est_ID')
+    ->join('tbl_cuarto as cua','hsc.Cuar_ID','=','cua.Cuar_ID')
+    ->join('tbl_usuario as usu','hsc.Usu_ID','=','usu.Usu_ID')
+    ->join('tbl_histo_asignaciones_aseo as hra','usu.Usu_ID','=','hra.Usu_ID')
+    ->join('tbl_asignacionaseo as asigA','hra.RolAseo_ID','=','asigA.RolAseo_ID')
+    ->join('tbl_tipoaseo as tipAseo','asigA.TipoAseo_ID','=','tipAseo.TipoAseo_ID')
+    ->select('per.Per_ID','per.per_Nombre','per.per_Apellido','tipAseo.tipoAseo_Nombre')
+    ->get();
+    return Datatables($data)->addColumn('btn',function($data){
+        return '<a href="#" id="'.$data->Per_ID.'" class="edit btn btn-primary" data-target="#ModalUpdate">Editar</a>';
+    })
+    ->rawColumns(['btn'])
+    ->toJson();       
 });
